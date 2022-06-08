670f16128ac4a0c461a60fc2e96baf02a4f47c68
sjhogge <sjhogge@gmail.com>
2022-06-07 21:30:20 -0500

    Initial commit

---
+++ src/main/java/com/brooklynotter/stucktogether/StuckTogether.java
@@ -0,0 +1,44 @@
+package com.brooklynotter.stucktogether;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+//Following this tutorial series: https://youtu.be/eqY17yWENEI
+//To RUN the Gradle build : go to the Gradle Tab -> Tasks -> forgegradle runs -> runClient
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(StuckTogether.MOD_ID)
+public class StuckTogether
+{
+    public static final String MOD_ID = "stucktogether";
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public StuckTogether() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
